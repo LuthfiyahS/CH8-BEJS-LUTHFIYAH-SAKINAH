01a7321d2f3842053c3f9ed4aaa97f5719908149
LuthfiyahS <luthfiyah.sakinah19@gmail.com>
2022-06-05 13:58:36 +0700

    Bismillah Deploy

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const app = require("./app")
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server Connent, run on port http://localhost:${process.env.PORT}`));
+app.listen(port, () => console.log(`Server Connent, run on port http://localhost:${port}`));
