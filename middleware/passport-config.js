edd0b11d2260b6533777556192a2694448880c4c
LuthfiyahS <luthfiyah.sakinah19@gmail.com>
2022-06-07 16:15:46 +0700

    Test  Deploy

--- middleware/passport-config.js
+++ middleware/passport-config.js
@@ -2,7 +2,9 @@
 require('dotenv').config();
 const { UserGames, UserGamesBiodata, UserGamesHistory } = require('./../models');
 const bcrypt = require("bcryptjs");
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
+const nodemailer = require("nodemailer");
+const htmlmail = require("./../controllers/handler/mail-template/welcome")
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
 
@@ -29,6 +31,35 @@ passport.use(new GoogleStrategy({
       if(!user){
         UserGames.create({uid: uid, username: profile.displayName, email:profile.email, role_id: 2, sign_with:'Google Account', google_id: profile.id})
         .then(user =>{
+            const email = user.email;
+            //Step 1: Creating the transporter
+            const transporter = await nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: '587',
+            service: "Gmail",
+            auth: {
+                user: process.env.USER_MAIL,
+                pass: process.env.PASSWORD_MAILAPP
+                }
+            });
+
+            //Step 2: Setting up message options
+            const messageOptions = {
+                from: process.env.USER_MAIL,
+                to: email,
+                subject: `Welcome to PIY Game State, ${user.username}`,
+                text: `Welcome to PIY Game State, ${user.username}`,
+                html: htmlmail
+            }
+
+            //Step 3: Sending email
+            transporter.sendMail(messageOptions, (err, info) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(info)
+                }
+            });
           return done(null, user)
         });
       }else{
